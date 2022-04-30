0bf86635ccf0eb359e14c8123983b7edadce6f8b
Dharmik Parmar <75156210+itsdharmik@users.noreply.github.com>
2022-04-30 02:48:57 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    
+<form action="append.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+    
+  </body>
+</html>
